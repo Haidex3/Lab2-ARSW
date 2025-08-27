4e223ea2958cb02c6edf8bb0203a479fdb4f539b
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2025-08-27 14:54:25 -0500

    re main

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,12 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 100);
 		pft.start();
 
-		System.out.println(pft.getPrimes());
 		
 		
 	}
