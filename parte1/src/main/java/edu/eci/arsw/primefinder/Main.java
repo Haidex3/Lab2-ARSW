b30a6c7de06099c82e544ae4ce638244cd165462
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2025-08-27 11:55:49 -0500

    parte1

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -2,16 +2,31 @@ package edu.eci.arsw.primefinder;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 100);
-		pft.start();
+    public static void main(String[] args) {
+        int max = 30000000;
+        int numThreads = 3;
+        int range = max / numThreads;
 
-		System.out.println(pft.getPrimes());
-		
-		
-	}
-	
+        // Crear arreglo para manejar los hilos
+        PrimeFinderThread[] threads = new PrimeFinderThread[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            int start = i * range;
+            int end = (i == numThreads - 1) ? max : (i + 1) * range;
+
+            threads[i] = new PrimeFinderThread(start, end);
+            threads[i].start();
+        }
+
+        // Esperar a que terminen todos los hilos
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Cálculo terminado con " + numThreads + " hilos.");
+    }
 }
